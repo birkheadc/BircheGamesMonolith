24b532a211a56d279a1b84a36d98dfbcc756d36f
Colby Birkhead <birkheadc@gmail.com>
2023-11-08 11:56:24 +0900

    fix dev.sh to run everything in current terminal, and close all services when interrupted

--- dev.sh
+++ dev.sh
@@ -11,32 +11,32 @@ for flag in "$@"
     case "$flag" in
       all)
         echo "Launching All Services And Homepage"
-        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5001" bash -c 'gnome-terminal -- dotnet run --project ./Authentication/src/Authentication/Authentication.csproj'
-        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5002" bash -c 'gnome-terminal -- dotnet run --project ./EmailVerification/src/EmailVerification/EmailVerification.csproj'
-        gnome-terminal -- bash -c 'cd ./Homepage; npm run start; exec bash'
-        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5003" bash -c 'gnome-terminal -- dotnet run --project ./UpdateUser/src/UpdateUser/UpdateUser.csproj'
-        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5000" bash -c 'gnome-terminal -- dotnet run --project ./UserRegistration/src/UserRegistration/UserRegistration.csproj'
+        npm run start --prefix Homepage/ &
+        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5001" dotnet run --project ./Authentication/src/Authentication/Authentication.csproj &
+        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5002" dotnet run --project ./EmailVerification/src/EmailVerification/EmailVerification.csproj &
+        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5003" dotnet run --project ./UpdateUser/src/UpdateUser/UpdateUser.csproj &
+        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5000" dotnet run --project ./UserRegistration/src/UserRegistration/UserRegistration.csproj &
         break
         ;;
       authentication)
         echo "Launching Authentication Service"
-        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5001" bash -c 'gnome-terminal -- dotnet run --project ./Authentication/src/Authentication/Authentication.csproj'
+        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5001" dotnet run --project ./Authentication/src/Authentication/Authentication.csproj &
         ;;
       email-verification)
         echo "Launching Email Verification Service"
-        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5002" bash -c 'gnome-terminal -- dotnet run --project ./EmailVerification/src/EmailVerification/EmailVerification.csproj'
+        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5002" dotnet run --project ./EmailVerification/src/EmailVerification/EmailVerification.csproj &
         ;;
       homepage)
         echo "Launching Homepage"
-        gnome-terminal -- bash -c 'cd ./Homepage; npm run start; exec bash'
+        npm run start --prefix Homepage/ &
         ;;
       update-user)
         echo "Launching Update User Service"
-        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5003" bash -c 'gnome-terminal -- dotnet run --project ./UpdateUser/src/UpdateUser/UpdateUser.csproj'
+        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5003" dotnet run --project ./UpdateUser/src/UpdateUser/UpdateUser.csproj &
         ;;
       user-registration)
         echo "Launching User Registration Service"
-        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5000" bash -c 'gnome-terminal -- dotnet run --project ./UserRegistration/src/UserRegistration/UserRegistration.csproj'
+        ASPNETCORE_ENVIRONMENT=Development ASPNETCORE_URLS="http://localhost:5000" dotnet run --project ./UserRegistration/src/UserRegistration/UserRegistration.csproj &
         ;;
       
     esac
@@ -45,3 +45,5 @@ echo ""
 echo "Finished!"
 echo ""
 echo ""
+trap 'pkill -P $$' SIGINT SIGTERM
+wait
