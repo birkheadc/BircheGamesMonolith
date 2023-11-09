4bcdb8db6b764cdca8c74e7d311a5beb534dfa0d
Colby Birkhead <birkheadc@gmail.com>
2023-11-09 11:09:44 +0900

    create generic version of response

--- Domain/Models/Responses/Response/Response.cs
+++ Domain/Models/Responses/Response/Response.cs
@@ -5,3 +5,8 @@ public class Response
   public bool WasSuccess { get; set; } = false;
   public List<ResponseError> Errors { get; set; } = new();
 }
+
+public class Response<T> : Response
+{
+  public T? Value { get; set; } = default;
+}
--- Domain/Models/Responses/ResponseBuilder/ResponseBuilder.cs
+++ Domain/Models/Responses/ResponseBuilder/ResponseBuilder.cs
@@ -57,3 +57,67 @@ public class ResponseBuilder
     return this;
   }
 }
+
+public class ResponseBuilder<T>
+{
+  private Response<T> _response = new();
+
+  public Response<T> Build()
+  {
+    return _response;
+  }
+
+  public ResponseBuilder<T> Succeed()
+  {
+    _response.WasSuccess = true;
+    return this;
+  }
+
+  public ResponseBuilder<T> Fail()
+  {
+    _response.WasSuccess = false;
+    return this;
+  }
+
+  public ResponseBuilder<T> WithError(ResponseError error)
+  {
+    _response.Errors.Add(error);
+    return this;
+  }
+
+  public ResponseBuilder<T> WithErrors(List<ResponseError> errors)
+  {
+    _response.Errors.AddRange(errors);
+    return this;
+  }
+
+  public ResponseBuilder<T> WithGeneralError(int? statusCode = null, string? message = null)
+  {
+    ResponseError error = new()
+    {
+      Field = null,
+      StatusCode = statusCode,
+      Message = message
+    };
+    _response.Errors.Add(error);
+    return this;
+  }
+
+  public ResponseBuilder<T> WithFieldError(string field, int? statusCode = null, string? message = null)
+  {
+    ResponseError error = new()
+    {
+      Field = field,
+      StatusCode = statusCode,
+      Message = message
+    };
+    _response.Errors.Add(error);
+    return this;
+  }
+
+  public ResponseBuilder<T> WithValue(T value)
+  {
+    _response.Value = value;
+    return this;
+  }
+}
